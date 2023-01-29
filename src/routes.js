5d418ea1a04922c5a0f46b5b7e90a848e011ad07
Shinichi Okada <okada.shin@gmail.com>
2023-01-29 15:51:27 +0900

    feat: add ForgotPassword

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ import Article from "./components/Article.svelte";
 import About from "./components/About.svelte";
 import NotFound from "./components/NotFound.svelte";
 import SignIn from './components/SignIn.svelte'
+import ForgotPassword from "./components/ForgotPassword.svelte";
+
 
 export const routes = {
   // Exact path
   "/": Home,
   "/about": About,
+  "/forgot-password": ForgotPassword,
   // Exact path + parameter
   "/article/:title": Article,
   "/auth/login": SignIn,
